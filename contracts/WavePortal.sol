198a38463ee34a90ed20f60db2e47ef6abf88576
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-10-17 23:43:20 +0200

    Add random function to decide winner

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,11 @@ contract WavePortal {
     uint256 totalWaves;
 
     /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
+
+    /*
      * A little magic, Google what events are in Solidity!
      */
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -32,26 +37,43 @@ contract WavePortal {
         console.log("I AM SMART CONTRACT. POG.");
     }
 
-    /*
-     * You'll notice I changed the wave function a little here as well and
-     * now it requires a string called _message. This is the message our user
-     * sends us from the frontend!
-     */
     function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        /*
+         * Generate a Psuedo random number between 0 and 100
+         */
+        uint256 randomNumber = (block.difficulty + block.timestamp + seed) %
+            100;
+        console.log("Random # generated: %s", randomNumber);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        /*
+         * Set the generated, random number as the seed for the next wave
+         */
+        seed = randomNumber;
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (randomNumber < 50) {
+            console.log("%s won!", msg.sender);
+
+            /*
+             * The same code we had before to send the prize.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     /*
